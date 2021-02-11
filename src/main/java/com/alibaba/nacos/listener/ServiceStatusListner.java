2b6bd3e81c386e7a57a42cc1e7a3b3eaf12ebc5d
fengandong <ufiread@sina.com>
2021-02-11 14:46:53 +0800

    配置redis

--- src/main/java/com/alibaba/nacos/listener/ServiceStatusListner.java
+++ src/main/java/com/alibaba/nacos/listener/ServiceStatusListner.java
@@ -39,25 +39,23 @@ public class ServiceStatusListner {
 
     //初始化监听服务上下线
     @PostConstruct
-    public void init(){
+    public void init() throws NacosException {
         // 每次ufire-websocket实例发生上线事件即更新redis
 
-        try{
-            namingService.subscribe(SERVICE_NAME, new EventListener() {
-                @Override
-                public void onEvent(Event event) {
-                    List<Instance> instances = ((NamingEvent) event).getInstances();
-                    instances.stream().forEach(instance -> {
-                        String host = instance.getIp() + ":" + instance.getPort();
-                        redisTemplate.opsForHash().put(SERVICE_NAME, host, HashRingUtil.getHash(host));
-                    });
-                    redisTemplate.convertAndSend(SERVICE_NAME, JSON.toJSONString(instances));
-                    System.out.println("监听到服务:" + SERVICE_NAME + " 发生变动" + JSON.toJSONString(instances));
-                }
-            });
-        }catch (Exception e){
-            System.out.println("发生异常"+e.getMessage()+e);
-        }
+
+        namingService.subscribe(SERVICE_NAME, new EventListener() {
+            @Override
+            public void onEvent(Event event) {
+                List<Instance> instances = ((NamingEvent) event).getInstances();
+                instances.stream().forEach(instance -> {
+                    String host = instance.getIp() + ":" + instance.getPort();
+                    redisTemplate.opsForHash().put(SERVICE_NAME, host, String.valueOf(HashRingUtil.getHash(host)));
+                });
+                redisTemplate.convertAndSend(SERVICE_NAME, JSON.toJSONString(instances));
+                System.out.println("监听到服务:" + SERVICE_NAME + " 发生变动" + JSON.toJSONString(instances));
+            }
+        });
+
 
     }
 
