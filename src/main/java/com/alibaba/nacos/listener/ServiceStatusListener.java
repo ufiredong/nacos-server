0b8e23e312dc025e24808f941592805f600778b4
fengandong <ufiread@sina.com>
2021-08-24 15:49:53 +0800

    1

--- src/main/java/com/alibaba/nacos/listener/ServiceStatusListener.java
+++ src/main/java/com/alibaba/nacos/listener/ServiceStatusListener.java
@@ -9,6 +9,7 @@ import com.alibaba.nacos.api.naming.listener.EventListener;
 import com.alibaba.nacos.api.naming.listener.NamingEvent;
 import com.alibaba.nacos.api.naming.pojo.Instance;
 import com.alibaba.nacos.util.HashRingUtil;
+import com.alibaba.nacos.util.SpringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,37 +28,26 @@ import java.util.*;
 @Component
 public class ServiceStatusListener {
     private static Logger logger = LoggerFactory.getLogger(ServiceStatusListener.class);
-    @Autowired
-    private RedisTemplate  redisTemplate;
-
-    @Autowired
-    private NamingService namingService;
-
     private final String SERVICE_NAME = "ufire-websocket";
-
     //初始化监听服务上下线
     @PostConstruct
     public void init() {
         try {
-            System.out.println("PostConstruct 执行");
+            Properties properties = new Properties();
+            properties.setProperty("serverAddr", "8.136.110.11:8848");
+            properties.setProperty("namespace", "public");
+            NamingService namingService = NamingFactory.createNamingService(properties);
+            RedisTemplate redisTemplate = (RedisTemplate) SpringUtil.getBean("redisTemplate");
             namingService.subscribe(SERVICE_NAME, new EventListener() {
                 @Override
                 public void onEvent(Event event) {
-                    System.out.println("onEvent");
                     List<Instance> instances = ((NamingEvent) event).getInstances();
-                    System.out.println(instances.size());
                     Boolean delete = redisTemplate.delete(SERVICE_NAME);
-                    System.out.println(delete.booleanValue());
-                    System.out.println("delete over");
                     instances.stream().forEach(instance -> {
                         String host = instance.getIp() + ":" + instance.getPort();
                         Integer hash = HashRingUtil.getHash(host);
-                        System.out.println(hash);
                         redisTemplate.opsForHash().put(SERVICE_NAME, hash.toString(), host);
-                        System.out.println("put over");
                     });
-                    System.out.println(instances.size());
-                    System.out.println("convertAndSend");
                     redisTemplate.convertAndSend(SERVICE_NAME, JSON.toJSONString(instances));
                     System.out.println("监听到服务:" + SERVICE_NAME + " 发生变动" + JSON.toJSONString(instances));
                 }
@@ -66,18 +56,5 @@ public class ServiceStatusListener {
             e.printStackTrace();
             System.out.println(e.getMessage());
         }
-
     }
-
-    @Bean
-    public NamingService getNamingService() throws NacosException {
-        System.out.println("初始化naming");
-        Properties properties = System.getProperties();
-        properties.setProperty("serverAddr", "172.66.0.10:8848");
-        properties.setProperty("namespace", "public");
-        NamingService naming = NamingFactory.createNamingService(properties);
-        System.out.println(naming.getServerStatus());
-        return naming;
-    }
-
 }
